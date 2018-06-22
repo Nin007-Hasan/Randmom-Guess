a8a64e5efe9b0a41f31af79b2479531879727d27
unifoxdigital <unifoxdigital.hihasan@gmail.com>
2018-06-23 00:27:23 +0600

    Kotlin File

--- app/src/main/java/com/hihasan/randmomguess/MainActivity.kt
+++ app/src/main/java/com/hihasan/randmomguess/MainActivity.kt
@@ -2,11 +2,22 @@ package com.hihasan.randmomguess
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_decide.setOnClickListener(){
+            println("I am Clicked")
+            Toast.makeText(this,"This is clicked Toast",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
+
